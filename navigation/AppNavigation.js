8e6a33c6dd8f1865475f31dea3316dfe60a6491a
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-03-29 07:27:43 +0200

    feat: finish setup

---
+++ navigation/AppNavigation.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import HomeScreen from '../screens/HomeScreen'
+const Stack = createNativeStackNavigator();
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+            headerShown: false
+        }}>
+            <Stack.Screen name='Home' component={HomeScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
